32d6a99c667f129aec0cb0da35a9b4db79c131c7
topthinking <topthinking@ximalaya.com>
2019-03-16 23:12:53 +0800

    2

--- hmr-middleware/server.js
+++ hmr-middleware/server.js
@@ -17,4 +17,10 @@ hmr([
   path.join(__dirname, './middleware/b'),
 ])(app);
 
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log(`
+打开浏览器 http://127.0.0.1:3000/
+尝试修改middleware文件夹内的js文件中ctx.body的内容
+再刷新浏览器，可以看到变化，服务端开发无需重启服务，完成热更新
+  `)
+});
